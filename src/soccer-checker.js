07feab25cc10c97833e87b095d9c0ac67f360e14
Julio Romano <julio.martins@e-core.com>
2020-03-15 13:16:28 -0300

    Fixing a bug related to the file paths containing special characters

--- src/soccer-checker.js
+++ src/soccer-checker.js
@@ -73,9 +73,7 @@ check = championshipName => {
         .end()
         .then(r => {
             if (r.matches.length) {
-                const filePath =
-                    "./data/." + r.championship.split(" ").join("");
-
+                const filePath = "./.data/." + convertToSlug(r.championship);
                 fs.appendFileSync(filePath, "");
                 const f = fs.readFileSync(filePath);
 
@@ -124,6 +122,21 @@ checkAll = () => {
         check(championships[i]);
     }
 };
+
+convertToSlug = text => {
+    const a = "àáäâãèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;";
+    const b = "aaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------";
+    const p = new RegExp(a.split("").join("|"), "g");
+    return text
+        .toString()
+        .toLowerCase()
+        .trim()
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special chars
+        .replace(/&/g, "-and-") // Replace & with 'and'
+        .replace(/[\s\W-]+/g, "-"); // Replace spaces, non-word characters and dashes with a single dash (-)
+};
+
+checkAll();
 
 schedule.scheduleJob("* * * * *", function() {
     checkAll();
